d4b81a7c1bd05e34a74b48324eaecb46fb01e2f6
Yehuda Shargal <yehuda.shargal@gmail.com>
2025-03-30 22:49:00 +0300

    fix add_edge in the Graph class

--- graph.py
+++ graph.py
@@ -1,5 +1,18 @@
+from collections import defaultdict
+
 class Graph:
-    def __init__(self, x):
-        self.x = x
-    def print_x(self):
-        print(self.x)
+    def __init__(self, n):
+        self.n = n
+        self.edges = defaultdict(list)
+
+    def add_edge(self, u, v):
+        if (0 <= u and u <= self.n-1) and (0 <= v and v <= self.n-1):
+            if u not in self.edges[v]:
+                self.edges[v].append(u)
+            if v not in self.edges[u]:
+                self.edges[u].append(v)
+        else:
+            print("numbers are not in range...")
+    
+    def print_edges(self):
+        print(self.edges)
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from graph import Graph
 
 def main():
-    pass
-
+    g = Graph(10)
+    g.add_edge(1,2)
+    g.add_edge(2,3)
+    g.add_edge(3,4)
+    g.add_edge(3,4)
+    g.add_edge(-1,2)
+    g.print_edges()
 if __name__ == "__main__":
     main()
